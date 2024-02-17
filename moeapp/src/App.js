cf82b1ff12d9cd30cd06e15e9dd76612c84bcab5
bryxnt1 <bryantperez56@gmail.com>
2024-02-17 16:01:53 -0500

    updates

--- moeapp/src/App.js
+++ moeapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import './App.css';
 
 function App() {
     return (
